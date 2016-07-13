f1791f15e2040e7ee0e8e6e6ac82e6aa71943940
Darylxyx <921218060@qq.com>
2016-07-13 21:52:07 +0800

    update

--- fs.js
+++ fs.js
@@ -1,8 +1,12 @@
 var fs = require('fs');
-fs.readFile('./app/index.html', 'utf-8', (err, data) => {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log(data);
-	}
-});
+// fs.readFile('./app/index.html', 'utf-8', (err, data) => {
+// 	if (err) {
+// 		console.log(err);
+// 	} else {
+// 		console.log(data);
+// 	}
+// });
+var file = fs.createReadStream('./app/index.html'),
+	out = fs.createWriteStream('./bundle/index.html');
+
+file.pipe(out);
