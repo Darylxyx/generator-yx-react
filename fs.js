55ff7eba04120f1b816559abd68eaf5707be4641
daryl <yuxiao.xie@symbio.com>
2016-07-14 11:55:29 +0800

    update

--- fs.js
+++ fs.js
@@ -1,8 +1,39 @@
 var fs = require('fs');
-fs.readFile('./app/index.html', 'utf-8', (err, data) => {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log(data);
-	}
+
+var config = fs.createReadStream('./webpack.config.js'),
+	inHtml = fs.createReadStream('./app/index.html'),
+	outHtml = fs.createWriteStream('./bundle/index.html');
+	// inCSS = fs.createReadStream('./bundle/bundle.css'),
+	// outCSS = fs.createWriteStream('./bundle/bundle1.css');
+
+config.on('data', (data) => {
+
+	var publicPath = (() => {
+
+		var dataStr = data.toString();
+
+		dataStr = dataStr.split('publicPath:')[1].split(',')[0].split('\'')[1];
+
+		return dataStr;
+
+	})();
+
+	// console.log(publicPath);
+
+	var pathReg = new RegExp(publicPath, 'g');
+	// console.log(pathReg);
+
+	inHtml.on('data', (data0) => {
+		outHtml.write(data0.toString().replace(pathReg, './'));
+	});
+
+	var cssData = fs.readFileSync('./bundle/bundle.css', 'utf8');
+	cssData = cssData.replace(pathReg, './');
+	
+	fs.writeFile('./bundle/bundle.css', cssData, (err) => {
+		if (err) {
+			console.log(err);
+		}
+	})
+
 });
