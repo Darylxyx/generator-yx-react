ea164f15628357a6d0612cb9ba1992afc83e5092
Darylxyx <921218060@qq.com>
2018-02-23 11:18:38 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var webpack = require('webpack'),
 	path = require('path');
 
 module.exports = {
-	entry: './src/index.js',
+	entry: ['babel-polyfill', './src/index.js'],
 	output: {
 		path: path.join(__dirname, 'dist'),
 		filename: 'bundle.js'
